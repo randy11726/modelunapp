5247a7e451080374253adbba5870f77477bb148f
Randy Ramon <randy11726@randys-mbp.dhcp.hofstra.edu>
2016-09-19 19:31:06 -0400

    added browse new

--- both/components/User/Browse.jsx
+++ both/components/User/Browse.jsx
@@ -15,7 +15,9 @@ Browse = React.createClass({
         return (
             <div>
             <div className="row">
-                <h4 className="text-center">Please select one of the following...</h4>
+                <h4 className="text-center">Please select one of the following...</h4> 
+                    <br>
+                    </br> 
                     <form onSubmit = {this.onSubmit} className="col offset-s4 s4">
                     <div className="row">      
                         <button className="waves-effect waves-light btn btn-block">Create a Committee</button>
