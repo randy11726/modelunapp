788038eb366f42ff811f8fb96f74eb3e18cbf349
Randy Ramon <randy11726@randys-mbp.dhcp.hofstra.edu>
2016-10-28 11:11:34 -0400

    we did iteration using map

--- both/components/User/Join.jsx
+++ both/components/User/Join.jsx
@@ -1,39 +1,51 @@
+
+
 Join = React.createClass({
-    
-    listC(){  // list of committe form data entries in DB 
-        
+
+    listC(){  // list of committe form data entries in DB
+
         return Committee.find().fetch();
-    }, 
-    
+    },
+
     onSubmit(e){
-        
+
         e.preventDefault();
         FlowRouter.go('/');
 
-            
+
         },
-    
-    
+
+
+
     render() {
         let comC = this.listC();
         if (comC.length < 1){
             return (<div> Loading Committees</div>)
         }
-    
+
+
+
         return (
-            
-                <div> 
-                    <div className="row col">
-                      <form>
-                        <div className="input-field">
-                          <input id="search" placeholder="Seach for a Committee" type="search" required/>
-                          <label for="search"><i className="material-icons">search</i></label>
-                          <i className="material-icons">close</i>
-                        </div>
-                      </form>
-                    </div>
-                    
-                    <table className="bordered">
+
+          <div>
+              <div className="row col">
+                <form>
+                  <div className="input-field">
+                    <input id="search" placeholder="Search for a Committee" type="search" required/>
+                    <label for="search"><i className="material-icons">search</i></label>
+                    <i className="material-icons">close</i>
+                  </div>
+                </form>
+              </div>
+
+          <ul>
+            {comC.map( (album) => {
+              return <div> {album.comName} </div>
+             })}
+
+        </ul>
+
+        {/*<table className="bordered">
         <thead>
           <tr>
               <th data-field="id">Committee Name</th>
@@ -44,11 +56,11 @@ Join = React.createClass({
 
         <tbody>
           <tr>
-            <td>{comC[2].comName} </td>
+            <td>{comC[0].comName} </td>
             <td>save bears that are on melting ice caps</td>
             <td>Click Me!</td>
           </tr>
-          <tr>
+          {/*<tr>
             <td>Guns</td>
             <td>Do we need stronger laws for the kids!</td>
             <td>Click Me!</td>
@@ -59,17 +71,17 @@ Join = React.createClass({
             <td>Click Me!</td>
           </tr>
         </tbody>
-      </table>
-                    
-                        
-      </div>              
-                  
-        );
-    
-    
-    }
-    
-    
-    
-});
+      </table>*/}
 
+
+      </div>
+
+
+        );
+
+
+    }
+
+
+
+});
