cd6c9658edea4c91e8a25e722e4bd767ce0a3fe7
Randy Ramon <randy11726@randys-mbp.dhcp.hofstra.edu>
2016-11-02 14:57:27 -0400

    dynamic table complete

--- both/components/User/Join.jsx
+++ both/components/User/Join.jsx
@@ -31,19 +31,42 @@ Join = React.createClass({
               <div className="row col">
                 <form>
                   <div className="input-field">
-                    <input id="search" placeholder="Search for a Committee" type="search" required/>
+                    <input id="search" autocomplete="false" placeholder="Search for a Committee" type="search" required/>
                     <label for="search"><i className="material-icons">search</i></label>
                     <i className="material-icons">close</i>
                   </div>
                 </form>
               </div>
 
-          <ul>
-            {comC.map( (album) => {
-              return <div> {album.comName} </div>
-             })}
+              <table className="bordered">
+              <thead className="tableHeader">
+                <tr>
+                    <th data-field="id">Committee Name</th>
+                    <th data-field="name">Topic</th>
+                    <th data-field="price">Details</th>
+                </tr>
+              </thead>
 
-        </ul>
+              <tbody>
+            {comC.map( (album) => {
+              return (
+
+
+
+
+                    <tr>
+                      <td> {album.comName} </td>
+                      <td>{album.comTopic}</td>
+                      <td><a className="btn-floating btn waves-effect waves-light blue"><i className="material-icons">info</i></a></td>
+                    </tr>
+
+
+
+          )
+             })}
+           </tbody>
+         </table>
+
 
         {/*<table className="bordered">
         <thead>
@@ -56,13 +79,15 @@ Join = React.createClass({
 
         <tbody>
           <tr>
+            {comC.forEach( (album) => {
+              return (<div> {album.comName} </div>)
+             })}
+          </tr>
+        </tbody>
+      </table> */}
+          {/*<tr>
             <td>{comC[0].comName} </td>
             <td>save bears that are on melting ice caps</td>
-            <td>Click Me!</td>
-          </tr>
-          {/*<tr>
-            <td>Guns</td>
-            <td>Do we need stronger laws for the kids!</td>
             <td>Click Me!</td>
           </tr>
           <tr>
