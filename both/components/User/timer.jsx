2137d003e83456604e65d0e58438283f8f733477
Randy Ramon <randy11726@randys-mbp.dhcp.hofstra.edu>
2016-11-28 11:24:19 -0500

    new timer nov 28 2016

--- both/components/User/timer.jsx
+++ both/components/User/timer.jsx
@@ -12,17 +12,28 @@ Timer = React.createClass({
     startTimer(event){
         //$("#DateCountdown").TimeCircles({start: "false"});
 
-        console.log("clicked start")
+        console.log("clicked start");
         console.log(event);
          },
+   editTimer(){
+
+
+
+       //console.log(time);
+        },
 
     componentDidMount() {
+
+      $(document).ready(function() {
+        $('select').material_select();
+      });
       var alarm = new buzz.sound('/sounds/alarm.mp3');
       var tick = new buzz.sound('/sounds/ticking.mp3');
       var stop = false;
       $("#startbutton").click(function(){
           $("#DateCountdown").TimeCircles().start();
           tick.play();
+          tick.loop();
           stop = false;
       });
       $("#stopbutton").click(function(){
@@ -33,6 +44,24 @@ Timer = React.createClass({
       $("#restartbutton").click(function(){
           $("#DateCountdown").TimeCircles().restart();
           tick.play();
+          tick.loop();
+          stop = false;
+      });
+      $("#morebutton").click(function(){
+          var timer = 60;
+          $("#DateCountdown").data('timer', timer);
+          $("#DateCountdown").TimeCircles().restart();
+          tick.play();
+          tick.loop();
+      });
+      $("#editButton").click(function(){
+          var newTime =$("#selectTime").val();
+          $("#DateCountdown").data('timer', newTime);
+          $("#DateCountdown").TimeCircles().restart();
+          $("#DateCountdown").TimeCircles().stop();
+          console.log(newTime);
+          console.log("clicked edit")
+          tick.stop();
       });
       $("#DateCountdown").TimeCircles({
           "start": false,
@@ -66,7 +95,7 @@ Timer = React.createClass({
       }).addListener(function()
       { //if ($("#DateCountdown").TimeCircles().getTime() > 1) { tick.play(); }
         if ($("#DateCountdown").TimeCircles().getTime() < 1) { alarm.play(); tick.stop();}
-      
+
       } );
 
 
@@ -78,13 +107,83 @@ Timer = React.createClass({
     render() {
 
 
+
+      var styleCard2 = {
+        textAlign: 'center'
+      };
+
+      var styleCard3 = {
+          width: 160,
+          position: 'relative',
+          textAlign: 'center'
+      };
+
+
         return (
             <div>
             <h1> Timer </h1>
-              <h8><div id="DateCountdown" data-timer="16" className="example"></div></h8>
-              <button id="startbutton" className="waves-effect waves-light btn-large">Start</button>
-              <button id="stopbutton" className="waves-effect waves-light btn-large">Stop</button>
-              <button id="restartbutton" className="waves-effect waves-light btn-large">Restart</button>
+              <div id="DateCountdown" data-timer="16" className="example"></div>
+
+
+
+              <div class="row" style ={styleCard2}>
+                <button id="startbutton" className="waves-effect waves-light btn-large bt1">Start</button>
+                <button id="stopbutton" className="waves-effect waves-light btn-large bt1 ">Stop</button>
+                <button id="restartbutton" className="waves-effect waves-light btn-large bt1">Restart</button>
+
+
+              </div>
+              <br>
+                  <br> </br>
+              </br>
+          
+
+
+
+              <div className="row" style ={styleCard2}>
+              <form className="col s12"style ={styleCard2}>
+                <div className="row">
+                  <div className="input-field col s6" style ={styleCard3}>
+                    <select id= "selectTime" style ={styleCard3}>
+                      <option value="" disabled selected>Select time!</option>
+                      <option value="120">2:00</option>
+                      <option value="240">4:00</option>
+                      <option value="360">6:00</option>
+                      <option value="600">10:00</option>
+                    </select>
+                    <label>Choose Timer Countdown</label>
+                  </div>
+                  <div className="input-field col s6">
+                    <button id="editButton" className="waves-effect waves-light btn-large"> Update Timer</button>
+                    <br>
+                        <br> </br>
+                    </br>
+                    <br>
+                        <br> </br>
+                    </br>
+                    <br>
+                        <br> </br>
+                    </br>
+                    <br>
+                        <br> </br>
+                    </br>
+                    <br>
+                        <br> </br>
+                    </br>
+                    <br>
+                        <br> </br>
+                    </br>
+                    <br>
+                        <br> </br>
+                    </br>
+                    <br>
+                        <br> </br>
+                    </br>
+                  </div>
+                </div>
+              </form>
+            </div>
+
 
 
 
